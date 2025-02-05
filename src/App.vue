95a4a44b801d563b802240f1d37cc55751ff4f92
ELRJOR <carlitos_04jorge@hotmail.com>
2025-02-04 20:28:16 -0600

    NavBar implementada

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-
 <template>
-
-  <h1>Hola XD</h1>
-
+  <div class="bg-[#111827] min-h-screen">
+    <NavBar />
+  </div>
 </template>
 
+<script setup>
+import { defineAsyncComponent } from 'vue';
+
+const NavBar = defineAsyncComponent(() => import('@/components/NavBar.vue'));
+</script>
