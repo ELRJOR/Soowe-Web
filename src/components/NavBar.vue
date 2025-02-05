1a5f22b34a4a9b37b6d4c8480811bf0cf3cbf071
ELRJOR <carlitos_04jorge@hotmail.com>
2025-02-04 20:54:27 -0600

    Imagenes y elementos modificados

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,9 @@
 <template>
     <header class="flex justify-between items-center p-6 bg-opacity-50 relative z-20">
-        <div class="text-white text-3xl font-bold">LOGO</div>
-        <div class="md:hidden z-30"></div>
+        <div class="flex items-center space-x-3">
+            <img src="@/assets/Logo.png" alt="Logo" class="w-10 h-10">
+            <span class="text-white text-3xl font-bold">Soowe</span>
+        </div>
         <button type="button" class="block focus:outline-none md:hidden"
         @click="isMenuOpen = !isMenuOpen">
             <span v-if="isMenuOpen" class="text-5xl">
@@ -30,12 +32,8 @@
 import { ref } from 'vue';
 
 const Menu = ref([
-    { name: 'Services', href: '#services' },
-    { name: 'About Me', href: '#about' },
-    { name: 'Skills', href: '#skills' },
-    { name: 'Projects', href: '#projects' },
-    { name: 'Testimonials', href: '#testimonials' },
-    { name: 'Contact', href: '#contact' }
+    { name: 'Registrarse', href: '#registrarse' },
+    { name: 'Iniciar Sesión', href: '#iniciarsesion' },
 ]);
 
 const isMenuOpen = ref(false);
