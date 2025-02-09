a839f6a58b5e38256953f7b4644705ed626be338
ELRJOR <ayala.04jorge@gmail.com>
2025-02-09 17:34:16 -0600

    Parte Finalizada Contacto

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -1,13 +1,9 @@
 <template>
     <section class="text-white mt-20" id="contact">
-        <h2 class="text-4xl font-bold text-white text-left mb-4 px-4 xl:pl-16">Let's Connect</h2>
+        <h2 class="text-4xl font-bold text-white text-left mb-4 px-4 xl:pl-16">Contacto</h2>
         <div class="grid md:grid-cols-2 gap-4 relative px-4 xl:px-16 mt-8" data-aos="zoom-in-up">
             <div>
-                <p class="text-[#adb7be]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta alias
-                    quibusdam officia at,
-                    a quis cumque ea aperiam omnis minima quam. Modi necessitatibus odit corporis at dolorem quae iusto
-                    aliquam?
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, porro!
+                <p class="text-[#adb7be]">Las grandes transformaciones comienzan con un paso. Si representas una organización y quieres sumarte a Soowe, completa los siguientes datos y nos pondremos en contacto contigo enseguida. ¡Únete al cambio ahora!
                 </p>
                 <div class="col-lg-4 col-md-4 mb-lg-0 mt-5">
                     <div class="flex mb-10 items-center">
@@ -17,8 +13,8 @@
                             <img src="https://img.icons8.com/metro/50/ffffff/new-post.png" alt="new-post" class="w-6">
                         </div>
                         <div class="ml-5 text-white">
-                            <h4>Email</h4>
-                            <p>xxxxxxx@gmail.com</p>
+                            <h4>Correo Electrónico</h4>
+                            <p>Soowecontacto@gmail.com</p>
                         </div>
                     </div>
                     <div class="flex mb-10 items-center">
@@ -28,8 +24,8 @@
                             <img src="https://img.icons8.com/ios-filled/50/ffffff/phone.png" alt="phone" class="w-6">
                         </div>
                         <div class="ml-5 text-white">
-                            <h4>Phone</h4>
-                            <p>+xxxxxxxxxx</p>
+                            <h4>Teléfono</h4>
+                            <p>9995074890</p>
                         </div>
                     </div>
                     <div class="flex mb-10 items-center">
@@ -41,7 +37,7 @@
                         </div>
                         <div class="ml-5 text-white">
                             <h4>LinkedIn</h4>
-                            <p>wwww.LinkedIn.com</p>
+                            <p>www.linkedin.com/company/soowe</p>
                         </div>
                     </div>
                 </div>
@@ -51,22 +47,22 @@
             overflow: hidden;border: 1px solid #111a3e;backdrop-filter: blur(9px);-webkit-backdrop-filter: blur(9px);">
             <form class="flex flex-col p-2" data-aos="zoom-in-up" >
                 <div class="mb-6">
-                    <label for="email" class="text-white block mb-2 text-sm font-medium">email</label>
+                    <label for="email" class="text-white block mb-2 text-sm font-medium">Correo Electrónico:</label>
                     <input type="email" id="email" class="bg-[#111827] placeholder:[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                     placeholder="email@gmail.com" name="email">
                 </div>
                 <div class="mb-6">
-                    <label for="subject" class="text-white block mb-2 text-sm font-medium">Subject</label>
+                    <label for="subject" class="text-white block mb-2 text-sm font-medium">Asunto:</label>
                     <input type="subject" id="subject" class="bg-[#111827] placeholder:[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                    placeholder="subject" name="subject">
+                    placeholder="Asunto" name="subject">
                 </div>
                 <div class="mb-6">
-                    <label for="message" class="text-white block mb-2 text-sm font-medium">Message</label>
+                    <label for="message" class="text-white block mb-2 text-sm font-medium">Mensaje:</label>
                     <textarea id="Message" class="bg-[#111827] placeholder:[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                    placeholder="Let's talk about ... " name="Message"></textarea>
+                    placeholder="Cuentanos más ... " name="Message"></textarea>
                 </div>
                 <button class="z-1 w-[100%!important] px-6 md:px-7 py-3 rounded-full sm:w-max flex justify-center text-white bg-primary border-2 border-transparent">
-                    Send Message
+                    Enviar Mensaje
                 </button>
             </form>
         </div>
