dce992247e618c1e3f9602d4f57d7ad26547046c
Jess4lxy <alex.mtz.rv17@gmail.com>
2025-02-18 15:38:28 -0600

    adding components to dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,3 +1,31 @@
 <template>
-
+    <!--Dashboard components-->
+    <div>
+        <NavBar />
+        <DashboardOverview />
+        <UserManagement />
+        <NurseManagement />
+        <AppointmentsSection />
+        <SettingsSection />
+        <Footer />
+    </div>
 </template>
+
+<script setup>
+    import { defineAsyncComponent } from 'vue';
+
+    const NavBar = defineAsyncComponent(() => import('@/components/DashboardComponents/NavBar.vue'));
+    const DashboardOverview = defineAsyncComponent(() => import('@/components/DashboardComponents/DashboardOverview.vue'));
+    const UserManagement = defineAsyncComponent(() => import('@/components/DashboardComponents/UserManagement.vue'));
+    const NurseManagement = defineAsyncComponent(() => import('@/components/DashboardComponents/NurseManagement.vue'));
+    const AppointmentsSection = defineAsyncComponent(() => import('@/components/DashboardComponents/AppointmentsSection.vue'));
+    const SettingsSection = defineAsyncComponent(() => import('@/components/DashboardComponents/SettingsSection.vue'));
+    const Footer = defineAsyncComponent(() => import('@/components/DashboardComponents/Footer.vue'));
+</script>
+
+<style>
+* {
+  scrollbar-width: thin;
+  scrollbar-color: #111827 #f1f1f1;
+}
+</style>
